3595e88b42b9ee3d8c736b374ba8bfee5ca0ad0e
ldelcaribe <laboratoriodelcaribe1@gmail.com>
2024-06-06 20:31:17 -0500

    nueva version 06062024.2

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.layouts')
 
 <script>
-  document.addEventListener("DOMContentLoaded", function() {
-    var boton = document.getElementById('nosotros');
-    var contenidoOriginal = boton.textContent; // Guarda el contenido original
+    document.addEventListener("DOMContentLoaded", function() {
+        var boton = document.getElementById('nosotros');
+        var contenidoOriginal = boton.textContent; // Guarda el contenido original
 
-    // Cambiar el contenido del elemento al cargar la página
-    boton.textContent = "NOSOTROS";
-    boton.classList.add("text-red-600");
+        // Cambiar el contenido del elemento al cargar la página
+        boton.textContent = "NOSOTROS";
+        boton.classList.add("text-red-600");
 
-    // Evitar que el enlace recargue la página
-    boton.addEventListener("click", function(event) {
-        event.preventDefault();
+        // Evitar que el enlace recargue la página
+        boton.addEventListener("click", function(event) {
+            event.preventDefault();
+        });
     });
-});
 </script>
 
 
@@ -31,18 +31,22 @@
 
         <div class="w-full flex flex-grow gap-x-0 ">
             <div class="w-[25vw] max-h-full ml-5 lg:ml-14 rounded-r-full pr-4 bg-red-600 hidden lg:block ">
-                <img class="w-[25vw] h-full rounded-r-full" src="{{ asset('images/nos_resena.jpg') }}" alt="" srcset="">
+                <img class="w-[25vw] h-full rounded-r-full" src="{{ asset('images/nos_resena.jpg') }}" alt=""
+                    srcset="">
             </div>
-            <p class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-xl  lg:ml-[3vw] pb-5 text-justify">
-                <span class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center">
+            <p class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-xl  lg:ml-[3vw] pb-3 text-justify">
+                <span class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center mb-2">
                     Reseña Historica
                 </span>
-                Laboratorio del caribe S.A.S. inició labores en Sincelejo el 20 de marzo de 1984. Se obtiene el primer
-                Registro Sanitario para el producto comercializado bajo la marca Artrixin iniciándose la expansión y
-                posicionamiento en todo el territorio Nacional.
-                <br />
-                A través de los años Laboratorio del Caribe S.A.S. ha crecido de manera importante en el mercado Nacional,
-                manteniendo una posición de liderazgo y prestigio ante el cuerpo médico por la calidad de sus productos.
+                <span class="leading-loose">
+                    Laboratorio del caribe S.A.S. inició labores en Sincelejo el 20 de marzo de 1984. Se obtiene el primer
+                    Registro Sanitario para el producto comercializado bajo la marca Artrixin iniciándose la expansión y
+                    posicionamiento en todo el territorio Nacional.
+                    <br />
+                    A través de los años Laboratorio del Caribe S.A.S. ha crecido de manera importante en el mercado
+                    Nacional,
+                    manteniendo una posición de liderazgo y prestigio ante el cuerpo médico por la calidad de sus productos.
+                </span>
             </p>
         </div>
 
@@ -51,26 +55,29 @@
         <div class="w-full flex flex-grow gap-x-0 ">
 
             <div class="w-[25vw] max-h-full ml-14  rounded-r-full pr-4 bg-red-600  hidden lg:block">
-                <img class="w-[26vw] h-full bg-white rounded-r-full " src="{{ asset('images/nos_politica_calidad.jpg') }}" alt=""
-                    srcset="">
+                <img class="w-[26vw] h-full bg-white rounded-r-full " src="{{ asset('images/nos_politica_calidad.jpg') }}"
+                    alt="" srcset="">
             </div>
 
             <p class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-xl lg:ml-[3vw]  pb-5 text-justify">
                 <span class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center">
                     Politícas de la calidad</span>
-
-
-                Laboratorio del Caribe S.A.S., es una empresa farmacéutica que está en un mejoramiento continuo, ofreciendo
-                productos y servicios confiables que cumplan con las normas de Buenas Prácticas de Manufactura, para
-                contribuir de esta forma al bienestar de la Sociedad.
-                <br />
-                Con ética y responsabilidad ofrecemos al mercado productos de excelente calidad, seguros y efectivos, que
-                satisfacen las necesidades del cliente, cumpliendo con normas Nacionales e Internacionales, y respetando el
-                medio ambiente.
-                <br />
-                A través de la gestión adecuada de los recursos y el compromiso asumido por nuestros colaboradores,
-                Laboratorio del Caribe S.A.S., genera el nivel de rentabilidad acorde a las expectativas de los socios,
-                permitiendo el desarrollo integral de los miembros de nuestra organización.
+                <span class="leading-loose">
+                    Laboratorio del Caribe S.A.S., es una empresa farmacéutica que está en un mejoramiento continuo,
+                    ofreciendo
+                    productos y servicios confiables que cumplan con las normas de Buenas Prácticas de Manufactura, para
+                    contribuir de esta forma al bienestar de la Sociedad.
+                    <br />
+                    Con ética y responsabilidad ofrecemos al mercado productos de excelente calidad, seguros y efectivos,
+                    que
+                    satisfacen las necesidades del cliente, cumpliendo con normas Nacionales e Internacionales, y respetando
+                    el
+                    medio ambiente.
+                    <br />
+                    A través de la gestión adecuada de los recursos y el compromiso asumido por nuestros colaboradores,
+                    Laboratorio del Caribe S.A.S., genera el nivel de rentabilidad acorde a las expectativas de los socios,
+                    permitiendo el desarrollo integral de los miembros de nuestra organización.
+                </span>
             </p>
         </div>
 
@@ -85,13 +92,17 @@
             <p class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-xl  lg:ml-[3vw]  pb-5 text-justify">
                 <span class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center">
                     Mision</span>
-
-                Laboratorio del Caribe S.A.S., es una empresa farmacéutica que tiene como misión, comercializar productos de
-                excelente calidad, cumpliendo con todas las Normas exigidas por las entidades que nos vigilan, respetando el
-                medio ambiente y generando de esta forma bienestar a la comunidad.
-                <br />
-                Laboratorio del Caribe S.A.S. propenderá por el desarrollo integral de sus colaboradores al mismo tiempo que
-                satisface las expectativas de sus socios.
+                <span class="leading-loose">
+                    Laboratorio del Caribe S.A.S., es una empresa farmacéutica que tiene como misión, comercializar
+                    productos de
+                    excelente calidad, cumpliendo con todas las Normas exigidas por las entidades que nos vigilan,
+                    respetando el
+                    medio ambiente y generando de esta forma bienestar a la comunidad.
+                    <br />
+                    Laboratorio del Caribe S.A.S. propenderá por el desarrollo integral de sus colaboradores al mismo tiempo
+                    que
+                    satisface las expectativas de sus socios.
+                </span>
             </p>
         </div>
         <!-- Estas son las lineas rojas -->
@@ -104,11 +115,14 @@
             <p class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-xl lg:ml-[3vw]  pb-5 text-justify">
                 <span class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center">
                     Vision</span>
-
-                Laboratorios del Caribe S.A.S. va a ser uno de los más reconocidos Laboratorios farmacéuticos de Colombia,
-                ampliando su línea de productos, para hacer una contribución a la sociedad, a través de sus productos,
-                construyendo herramientas para ser más competitivos y posicionarse como uno de los principales laboratorios
-                farmacéuticos en las regiones donde hace presencia.
+                <span class="leading-loose">
+                    Laboratorios del Caribe S.A.S. va a ser uno de los más reconocidos Laboratorios farmacéuticos de
+                    Colombia,
+                    ampliando su línea de productos, para hacer una contribución a la sociedad, a través de sus productos,
+                    construyendo herramientas para ser más competitivos y posicionarse como uno de los principales
+                    laboratorios
+                    farmacéuticos en las regiones donde hace presencia.
+                </span>
             </p>
         </div>
 
@@ -116,32 +130,38 @@
         @include('layouts.lineasrojas')
         <div class="w-full flex flex-grow gap-x-0">
             <div class="w-[25vw] max-h-full ml-14  rounded-r-full pr-4 bg-red-600  hidden lg:block">
-                <img class="w-full h-full  rounded-r-full" src="{{ asset('images/nos_valores_corporativos.jpg') }}" alt=""
-                    srcset="">
+                <img class="w-full h-full  rounded-r-full" src="{{ asset('images/nos_valores_corporativos.jpg') }}"
+                    alt="" srcset="">
             </div>
-            <p class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-lg lg:ml-[3vw]  pb-5 text-justify">
-                <span class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center">
-                    Valores corporativos</span>
-                <br />
-                <span class="font-bold">Respeto: </span> Aceptamos las decisiones y acciones de los clientes internos y
-                externos siendo tolerantes.
-                <br />
-                <span class="font-bold">Compromiso:</span> Damos lo máximo de cada uno de nosotros con sentido de
-                pertenencia hacia la empresa.
-                <br />
-                <span class="font-bold">Honestidad:</span> Actuamos conforme a los intereses comunes y no particulares de
-                acuerdo a la ley y las costumbres.
-                <br />
-                <span class="font-bold">Lealtad:</span> Somos fieles frente a los compromisos y objetivos de la empresa.
-                <br />
-                <span class="font-bold">Responsabilidad: </span> Somos conscientes de nuestras actuaciones, cumpliendo
-                cabalmente de forma que se logren los objetivos de la empresa.
-
-            </p>
+            <div class="px-7 w-full lg:mr-10 flex flex-col justify-center  md:text-lg lg:ml-[3vw]  pb-3 text-justify">
+                <p class="font-bold text-red-600 md:text-2xl lg:text-3xl lg:text-center mb-2">
+                    Valores corporativos
+                </p>
+                <span class="flex flex-row mb-2">
+                    <strong class="mr-2">Respeto:</strong>Aceptamos las decisiones y acciones de los clientes internos y
+                    externos siendo tolerantes.
+                </span>
+                <span class="flex flex-row mb-2">
+                    <strong class="mr-2"> Compromiso:</strong> Damos lo máximo de cada uno de nosotros con sentido de
+                    pertenencia hacia la empresa.
+                </span>
+                <span class="flex flex-row mb-2">
+                    <strong class="mr-2">Honestidad:</strong> Actuamos conforme a los intereses comunes y no particulares
+                    de
+                    acuerdo a la ley y las costumbres.
+                </span>
+                <span class="flex flex-row mb-2">
+                    <strong class="mr-2">Lealtad:</strong> Somos fieles frente a los compromisos y objetivos de la
+                    empresa.
+                </span>
+                <span class="flex flex-row mb-2">
+                    <strong class="mr-2">Responsabilidad: </strong> Somos conscientes de nuestras actuaciones, cumpliendo
+                    cabalmente de forma que se logren los objetivos de la empresa.
+                </span>
+            </div>
         </div>
         <!-- Estas son las lineas rojas -->
         @include('layouts.lineasrojas')
     </div>
     @include('layouts.footer')
 @endsection
-
--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -19,7 +19,7 @@
     <div>
         <div class="pt-5 pb-20 ">
             <div
-                class="w-full flex justify-center text-center text-lg sm:text-2xl  lg:text-4xl xl:text-5xl text-red-600 font-titulo1">
+                class="w-full flex justify-center text-center text-sm sm:text-lg  lg:text-3xl text-red-600 font-titulo1">
                 <span class="">Nuestros</span>
                 <span class="mx-3 md:mx-6">Productos</span>
             </div>
@@ -31,12 +31,12 @@
                     <div class="w-full h-auto bg-gray-200 pr-1" style="border-radius: 3vw;">
                         <div class="w-full h-full bg-white border-x-2  border-dotted border-gray-300 m-2 p-3"
                             style="border-top-left-radius: 4vw; border-top-right-radius: 4vw; border-bottom-left-radius: 3.5vw; border-bottom-right-radius: 3.5vw;">
-                            <div class="max-w-full  h-35 flex flex-row justify-center text-center">
+                            <div class="w-auto  h-48 flex flex-row justify-center text-center">
                                 <img class="border-0" src="{{ asset('imagen/' . $producto->imagen) }}"
                                     style="border-radius: 3vw;" alt="{{ $producto->nombre }}">
                             </div>
                             <h1
-                                class="max-w-full text-2xl lg:text-xl  2xl:text-3xl flex flex-row justify-center text-center text-red-600 font-mote font-bold">
+                                class="max-w-full text-2xl lg:text-xl  xl:text-2xl flex flex-row justify-center text-center text-red-600 font-mote font-bold">
                                 {{ $producto->nombre }}
                             </h1>
                             <h1 class="max-w-full text-xl lg:text-lg flex flex-row justify-center text-center font-normal ">
@@ -64,27 +64,27 @@
                                     </button>
 
 
-                                    <div id="noficha" class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto h-auto  flex-col hidden">
-                                        <div class="w-full lg:w-sm h-auto flex flex-col space-y-3 justify-center items-center text-center ">
+                                    <div id="noficha" class="fixed w-xs h-auto  top-[200px]  left-[20px] right-[20px] flex-col hidden">
+                                        <div class="w-auto lg:w-sm h-auto flex flex-col space-y-3 justify-center items-center text-center">
                                             <div class="bg-white w-auto lg:w-sm border-2 border-red-600 rounded-3xl shadow-2xl shadow-red-200  p-2 lg:p-5 ">
-                                                <h1 class="flex flex-row mb-5 text-justify text-red-600 text-lg lg:text-2xl border-b-2 border-red-400">
-                                                    <span>Para control funcional de la pagina laboratorio del caribe le
+                                                <h1 class="flex flex-row mb-5 justify-center text-center text-red-600 text-sm lg:text-lg border-b-2 border-red-400">
+                                                    <span>Control funcional plataforma <br/> Laboratorio del caribe le
                                                         informa.
                                                     </span>
                                                 </h1>
-                                                <div class="text-black text-sm lg:text-lg font-bold">
-                                                    Para poder ver las fichas técnicas de los productos, debe realizar los siguientes pasos:
+                                                <div class="text-black text-xs lg:text-sm font-bold">
+                                                    Para poder ver las fichas debe:
                                                 </div>
 
-                                                <div class="text-gray-400 flex flex-col pl-2 p-1 lg:pl-20 lg:p-2 text-sm lg:text-lg">
-                                                    <div class="text-left p-2">1. - Registrese en la opcion Usuario</div>
-                                                    <div class="text-left p-2">2. - Inicie sesión con usuario registrado</div>
-                                                    <div class="text-left p-2">3. - Seleccione nuevamente ficha del producto a ver</div>
+                                                <div class="text-gray-400 text-left flex flex-col pl-2 lg:p-2 text-xs lg:text-sm">
+                                                    <div >1. - Registrese en la opcion Usuario</div>
+                                                    <div >2. - Inicie sesión con usuario registrado</div>
+                                                    <div >3. - Seleccione nuevamente ficha del producto a ver</div>
                                                 </div>
 
                                                 <div class="flex flex-row space-x-20 justify-center">
                                                     <button onclick="clicknoficha()"
-                                                        class="text-center mt-10 w-auto h-auto p-2 bg-gray-100 text-black/60 rounded-3xl hover:text-black/100 border-2 border-red-600 hover:bg-gray-400">
+                                                        class="text-center mt-3 w-auto h-auto px-2 bg-gray-100 text-black/60 rounded-3xl hover:text-black/100 border-2 border-red-600 hover:bg-gray-400">
                                                         Cerrar
                                                     </button>
                                                 </div>
